4d8b4b68f9db9c78274ea12177852bf2bb7816a4
MarcusMcKenzie <46785875+MarcusMcKenzie@users.noreply.github.com>
2020-05-08 10:44:07 -0400

    Add files via upload

---
+++ motion_samples_ws/build/hsrb_motion_samples/CMakeFiles/_run_tests_hsrb_motion_samples_rostest_tests_arm_action.test.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_run_tests_hsrb_motion_samples_rostest_tests_arm_action.test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/_run_tests_hsrb_motion_samples_rostest_tests_arm_action.test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ motion_samples_ws/build/hsrb_motion_samples/CMakeFiles/omni_message_velocity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/omni_message_velocity.dir/src/omni_message_velocity.cpp.o"
+  "/home/marcus/motion_samples_ws/devel/lib/hsrb_motion_samples/omni_message_velocity.pdb"
+  "/home/marcus/motion_samples_ws/devel/lib/hsrb_motion_samples/omni_message_velocity"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/omni_message_velocity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
